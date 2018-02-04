6e41b74dbc0c87582f2358f0755ffdfc31c2f996
kelnage <shiny.nickel@gmail.com>
2018-02-04 17:05:53 +0000

    Remove iv field from database (no longer used)

--- webservice/database.sql
+++ webservice/database.sql
@@ -25,7 +25,6 @@ DROP TABLE IF EXISTS `Message`;
 CREATE TABLE `Message` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `encbody` varchar(65000) DEFAULT NULL,
-  `iv` char(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
